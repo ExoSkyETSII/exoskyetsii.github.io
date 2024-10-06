41786c3fd200c6f68c41f5804722895bb039e307
Angelo Moraschi <68395930+amoraschi@users.noreply.github.com>
2024-10-06 23:08:33 +0200

    Alerts

---
+++ src/alert.js
@@ -0,0 +1,15 @@
+document.getElementById('news').onclick = function() {
+  alert('Coming soon! In this tab, the latest news in astronomy and space exploration will be published for our users.');
+};
+document.getElementById('login').onclick = function() {
+  alert("In the near future, you will be able to create an account linked to your social media to track the progress you make on the website. This will allow you to save all the exoplanets you discover and have a status bar to level up and reach all the stars!");
+};
+document.getElementById('search').onclick = function() {
+  alert("Under Construction");
+};
+document.getElementById('faq').onclick = function() {
+  alert("Under Construction");
+};
+document.getElementById('contact').onclick = function() {
+  alert("Cooming Soon! Reach us on our email for any queries: forgeconstellation@gmail.com ");
+};
